87bf5053a5b8e403d4c3cd954d4d1b74175470cd
Galihwahyu Arofah <wahyuuarofah@gmail.com>
2024-03-08 21:05:16 +0700

    praktikum 2.3 (js4)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,14 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 3)->count();
+        $user = UserModel::where('level_id', 2)->count();
         //dd($user);
         return view('user', ['data' => $user]);
     }
-}
+}   
+
 /*$user = UserModel::findOr(20,['username', 'nama'], function(){abort(404);});*/
 //$user = UserModel::find(1);
 //$user = UserModel::where('level_id', 1)->first();
 //$user = UserModel::firstWhere('level_id', 1);
 //$user = UserModel::findOr(1,['username', 'nama'], function(){abort(404);*/
-//$user = UserModel::where('level_id', 2)->count();
-//dd($user);
-//$user = UserModel::findOrFail(1);
