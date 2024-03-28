8a86dd84c24d2200afbcf20de4762739e0d5d357
Galihwahyu Arofah <wahyuuarofah@gmail.com>
2024-03-28 13:50:30 +0700

    praktikm 1 js 7

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+
+class   WelcomeController extends Controller
+{
+    public function index()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Selamat Datang',
+            'list' => ['Home', 'Welcome']
+        ];
+
+        $activeMenu = 'dashboard';
+
+        return view('Welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+
+    }
+}
