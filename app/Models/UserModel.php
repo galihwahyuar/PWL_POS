a92a1c130f6370025d39bd16e20d18358a42a8cd
Galihwahyu Arofah <wahyuuarofah@gmail.com>
2024-05-27 17:11:09 +0700

    JS 11

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,10 +5,11 @@ namespace App\Models;
 //use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 //use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Fondation\Auth\User as Authenticatable;;
 
-class UserModel extends Model
+class UserModel extends Authenticatable implements JWTSubject
 {
     public function getJWTIdentifier(){
         return $this->getKey();
@@ -21,10 +22,23 @@ class UserModel extends Model
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = [
+        'username',
+        'nama',
+        'password',
+        'level_id',
+        'image' //tambahan
 
-    /*public function level(): BelongsTo
+    ];
+
+    public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }*/
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' .$image),
+        );
+    }
 }
