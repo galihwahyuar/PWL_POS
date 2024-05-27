642cd133b5754141fee5d5fa04f4ba032dfa844e
Galihwahyu Arofah <wahyuuarofah@gmail.com>
2024-05-27 15:11:09 +0700

    Praktikum 2 JS 10

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 //use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+//use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Fondation\Auth\User as Authenticatable;;
 
 class UserModel extends Model
@@ -23,8 +23,8 @@ class UserModel extends Model
 
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function level(): BelongsTo
+    /*public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
+    }*/
 }
